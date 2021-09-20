a6a5efaf41e24661e29e2f4c1b2db530543d0089
oviet <47376511+oviet@users.noreply.github.com>
2021-09-20 12:38:05 +0700

    Update repository.sh
    wrap it

--- Oviet/repository.sh
+++ Oviet/repository.sh
@@ -1,2 +1,12 @@
-ansible node4.example.com  -u root -k -m yum_repository -a 'name="BaseOS" description="Repo Beta RHEL8 - BaseOS" baseurl=https://downloads.redhat.com/redhat/rhel/rhel-8-beta/baseos/x86_64/ gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-beta,file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release gpgcheck=true enabled=true'
-ansible node4.example.com -u root -k -m yum_repository -a 'name="AppStream" description="Repo Beta RHEL8 - AppStream" baseurl=https://downloads.redhat.com/redhat/rhel/rhel-8-beta/appstream/x86_64/ gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-beta,file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release gpgcheck=true enabled=true'
+yum_repository -a 'name="BaseOS" description="Repo Beta RHEL8 - BaseOS" 
+baseurl=https://downloads.redhat.com/redhat/rhel/rhel-8-beta/baseos/x86_64/ 
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-beta,_
+file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release 
+gpgcheck=true 
+enabled=true'
+yum_repository -a 'name="AppStream" description="Repo Beta RHEL8 - AppStream" 
+baseurl=https://downloads.redhat.com/redhat/rhel/rhel-8-beta/appstream/x86_64/ 
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-beta,_
+file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release 
+gpgcheck=true 
+enabled=true'
